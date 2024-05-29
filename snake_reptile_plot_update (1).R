5fe4b656e2f513da559248d601ed4c0123ce0254
Chong <chong.wan@mail.utoronto.ca>
2024-05-29 16:57:00 -0400

    Finished Task

--- snake_reptile_plot_update (1).R
+++ snake_reptile_plot_update (1).R
@@ -7,10 +7,10 @@ dnds <- read.csv("Snake_Reptile_CmC (1).csv")
 
 x_min <- rep(-Inf, length(dnds_line$Gene_name))
 x_max <- rep(Inf, length(dnds_line$Gene_name))
-y_min <- 0:25
-y_max <- 1:26
-y_min <- y_min + 0.5
-y_max <- y_max + 0.5
+# y_min <- 0:length(dnds_line$Gene_name)
+# y_max <- 1:length(dnds_line$Gene_name)
+# y_min <- y_min + 0.5
+# y_max <- y_max + 0.5
 
 # Transforms to wide format for plotting lines
 dnds_line <- spread(dnds, key = Clade, value = dNdS)
@@ -26,7 +26,7 @@ for (i in 1:length(snake_col)){
     new_col <- c(new_col, "insignificant")
   }
   else{
-    if (abs(snake_col[i] - reptile_col[i]) > 0.3){
+    if (abs(snake_col[i] - reptile_col[i]) > 0.31){
       new_col <- c(new_col, "significant")
     }
   }
@@ -36,6 +36,22 @@ dnds_line$Absolute_Diff <- new_col
 
 ## Plots the dN/dS values for each clade by gene and grouped by photoreceptor class
 ggplot() +
+  
+  # geom_rect(
+  #   data = dnds_line,
+  #   aes(xmin = x_min, xmax = x_max, ymin = rep(0, length(Gene_name)), ymax = Gene_name,
+  #       fill = as.factor(Absolute_Diff)),
+  #   alpha = 0.5 # Make the background semi-transparent
+  # ) +
+  
+  geom_segment(
+    data = dnds_line,
+    aes(x = rep(-Inf, length(Gene_name)), 
+        y = Gene_name, 
+        xend = rep(Inf, length(Gene_name)), 
+        yend = Gene_name, 
+        colour = Absolute_Diff), 
+    size=2.5) +
 	# Plots lines between dN/dS values
 	geom_segment(
 		data = dnds_line,
@@ -45,7 +61,6 @@ ggplot() +
 				yend = Gene_name, 
 				colour = Photoreceptor), 
 			size=2.5) + labs(x = "\u03C9 (dN/dS)") +
-  
 
   # Plots the dN/dS for both clades as points
   geom_point(
@@ -55,31 +70,24 @@ ggplot() +
         fill=Clade),
     size=2.5, 
     shape = 21) +
-  
-  geom_rect(
-    data = dnds_line,
-    aes(xmin = x_min, xmax = x_max, ymin = y_min, ymax = y_max,
-        fill = as.factor(Absolute_Diff)),
-    alpha = 0.5 # Make the background semi-transparent
-  ) +
 
   
 	# Colour scheme for the different photoreceptor classes
-	scale_color_manual(values=c('red', 'green', 'blue')) +
+	scale_color_manual(values=c('red', 'green', '#DCDCDC', 'blue', '#808080')) +
 	# Coloiur scheme for the background and foreground dN/dS
-	scale_fill_manual(values=c('lightgrey','darkgrey','white','black')) +
+	scale_fill_manual(values=c('white','black')) +
 	# Groups data by photoreceptor class
 	
-  # facet_grid(scales="free_y",space="free_y", facets = Photoreceptor ~ .) +
-	
+  facet_grid(scales="free_y", space = "free_y", facets = Photoreceptor ~.) +
+  
   # Scales axis and sets the aesthetics for the chart
-	scale_x_continuous(limits=c(0, 2),breaks=seq(0, 2, 1)) +
+	scale_x_continuous(n.breaks = 6) +
 	theme(axis.text.x = element_text(angle = 0, hjust = 0.5), 
 		panel.background = element_rect(fill = "white"),
 		# the background of the data, want to change to grey white if insignificant
-		panel.grid.major = element_line(size = 3, colour = "lightgrey"), 
+		panel.grid.major = element_line(size = 3, colour = "white"), 
 		panel.grid.minor.x = element_blank(),
-		panel.grid.major.x = element_line(colour = "white",size = 0.5),
+		panel.grid.major.x = element_line(colour = "black",size = 0.5),
 		axis.ticks.x = element_blank(),
 		axis.ticks.y = element_blank(),
 		axis.title.y = element_blank()) 
